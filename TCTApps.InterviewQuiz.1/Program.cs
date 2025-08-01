88388491ef840956dfa963ab21fa1142a7e4106d
Liew Jun Kang <ljk_jgs@hotmail.com>
2025-08-01 14:46:53 +0800

    initial

---
+++ TCTApps.InterviewQuiz.1/Program.cs
@@ -0,0 +1,19 @@
+﻿//What is the issue/risk for this program?
+//How to improve?
+
+class Calculator
+{
+    public double Divide(int a, int b)
+    {
+        return a / b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var calc = new Calculator();
+        Console.WriteLine("Result: " + calc.Divide(10, 2));
+    }
+}
---
+++ TCTApps.InterviewQuiz.2/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = { 1, 2, 4, 5 };
+        Console.WriteLine("Missing number is: " + FindMissingNumber(input));
+    }
+
+    static int FindMissingNumber(int[] numbers)
+    {
+        // TODO: Implement the logic
+        return -1;
+    }
+}
